ce895e7e4cc955da7db0c82223514dc69b05a9cd
yash-nihalani-byjus <yash.nihalani@byjus.com>
2023-01-27 13:06:48 +0530

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#   root "articles#index"
+#    get "/articles", to: "articles#index"
+#    get "/articles/:id", to: "articles#show"
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "articles#index"
+
+  resources :articles
 end
