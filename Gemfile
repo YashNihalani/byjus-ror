da45804459b7c98689d5a6fa34bcae3e91a1e119
yash-nihalani-byjus <yash.nihalani@byjus.com>
2023-02-02 13:16:54 +0530

    added auth with devise

--- Gemfile
+++ Gemfile
@@ -34,7 +34,10 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 # Reduces boot times through caching; required in config/boot.rb
+
 gem 'bootsnap', '>= 1.1.0', require: false
+
+gem "devise" 
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 # Rails.application.routes.draw do
+  # devise_for :users
 #   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 #   root "articles#index"
@@ -7,12 +8,14 @@
 # end
 
 # Rails.application.routes.draw do
+  # devise_for :users
 #   root "articles#index"
 
 #   resources :articles
 # end
 
 Rails.application.routes.draw do
+  devise_for :users
   root "articles#index"
 
   resources :articles do
